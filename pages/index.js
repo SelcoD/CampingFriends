2b31d1b053598e5c712b9daaacf1bb1e17cf18d7
Selco Drapalla <gargamelsave90@gmail.com>
2023-07-07 14:48:28 +0200

    change the folder-structure

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,14 @@
 import React from "react";
+import CampingCard from "../components/CampingCard";
+import { campingTrips } from "../data/campingTrips";
 
 export default function HomePage() {
   return (
     <div>
-      <h1>Camping Friends</h1>
+      <h1>Organize your Camping Trips</h1>
+      {campingTrips.map((trip) => (
+        <CampingCard key={trip.id} trip={trip} />
+      ))}
     </div>
   );
 }
