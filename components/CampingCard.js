8b9865ae088a82b83a51b7a78e20a370e15a3fd9
Selco Drapalla <gargamelsave90@gmail.com>
2023-07-08 17:22:06 +0200

    changed Card to div

--- components/CampingCard.js
+++ components/CampingCard.js
@@ -3,11 +3,11 @@ import { Card } from "../styles/styles";
 
 const CampingCard = ({ trip }) => {
   return (
-    <Card>
+    <div>
       <h2>{trip.location}</h2>
       <p>Date: {trip.date}</p>
       <p>Friends: {trip.friends.join(", ")}</p>
-    </Card>
+    </div>
   );
 };
 
