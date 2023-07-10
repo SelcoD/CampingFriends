c24bdefe05a8264bac26be103ccbdf4ede4cf49c
Selco Drapalla <gargamelsave90@gmail.com>
2023-07-10 09:53:21 +0200

    try to fix vercel bug

--- pages/_document.js
+++ pages/_document.js
@@ -1,8 +1,6 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
 import { ServerStyleSheet } from "styled-components";
 
-import GlobalStyles from "../styles/styles.js";
-
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
     const sheet = new ServerStyleSheet();
