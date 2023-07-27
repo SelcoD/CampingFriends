de4ec1867d0b1b8f0358cbf0ad413af16f10572b
Selco Drapalla <gargamelsave90@gmail.com>
2023-07-27 10:26:41 +0200

    fixed the commentare from the code review

--- styles/styles.js
+++ styles/styles.js
@@ -6,7 +6,7 @@ export const Container = styled.div`
   padding: 20px;
 `;
 
-export const Card = styled.div`
+export const Card = styled.article`
   background-color: #ffffff;
   box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1);
   padding: 0px;
@@ -23,7 +23,7 @@ export const Card = styled.div`
   justify-content: center;
 `;
 
-export const Header = styled.header`
+export const Header = styled.main`
   padding: 20px;
   color: black;
   width: 100%;
