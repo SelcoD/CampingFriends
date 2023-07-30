36dfb3fb2de7e38a221cb4a6540261323d3e951d
Selco Drapalla <gargamelsave90@gmail.com>
2023-07-30 21:19:44 +0200

    change back to listpage button to svg

--- components/EditCard.js
+++ components/EditCard.js
@@ -157,7 +157,16 @@ export default function EditCard({ currentTrip, onEditTrip }) {
         </StyledEditCard>
         <FooterContainer>
           <CenteredLink href="/">
-            <StyledListPageButton>Back to List Page</StyledListPageButton>
+            <StyledListPageButton>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                height="48"
+                viewBox="0 -960 960 960"
+                width="48"
+              >
+                <path d="m274-450 248 248-42 42-320-320 320-320 42 42-248 248h526v60H274Z" />
+              </svg>
+            </StyledListPageButton>
           </CenteredLink>
         </FooterContainer>
       </Container>
